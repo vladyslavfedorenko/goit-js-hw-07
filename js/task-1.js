86c76a3f5758d9d0be3ef2c3976cec5d27149f1c
vladyslav_fedorenko <godforgiveme1993@gmail.com>
2025-06-20 10:55:45 +0300

    compleded task-1

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector("#categories");
+
+const categoryItems = categoriesList.querySelectorAll(".item");
+console.log("Number of categories:", categoryItems.length);
+
+categoryItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelectorAll("ul li").length;
+
+  console.log("Category:", categoryTitle);
+  console.log("Elements:", elementsCount);
+});
+// categoriesList.style.fontWeight = "600";
